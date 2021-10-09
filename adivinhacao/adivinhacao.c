0a2e22527fde0e65cafbe59d199136c9472b2eaf
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-10-09 12:38:37 -0300

    Inclui avaliação do input do usuário

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -12,5 +12,18 @@ int main(){
 
     printf("Qual é o seu chute? ");
     scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    
+    int acertou = (chute == numerosecreto);
+
+    if(acertou){
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue de novo! Você é um bom jogador!\n");
+    } else {
+        int maior = (chute > numerosecreto);
+        if (maior){
+            printf("Seu chute foi maior que o número secreto!\n");
+        } else {
+            printf("Seu chute foi menor que o número secreto!\n");
+        }
+    }
 }
