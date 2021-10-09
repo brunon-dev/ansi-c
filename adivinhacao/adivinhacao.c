12b519837de4871113e2b70a82f5ada9367ae6c7
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-10-09 19:55:29 -0300

    Inclui número de tentativas de chute

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main(){
     // imprime cabeçalho do jogo
     printf("******************************************\n");
@@ -10,20 +12,24 @@ int main(){
 
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    
-    int acertou = (chute == numerosecreto);
-
-    if(acertou){
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo! Você é um bom jogador!\n");
-    } else {
+    for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
+        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        
+        int acertou = (chute == numerosecreto);
         int maior = (chute > numerosecreto);
-        if (maior){
+
+        if(acertou){
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo! Você é um bom jogador!\n");
+            break;
+        } else if (maior){
             printf("Seu chute foi maior que o número secreto!\n");
         } else {
             printf("Seu chute foi menor que o número secreto!\n");
         }
     }
+
+    printf("Fim de jogo!\n");
 }
