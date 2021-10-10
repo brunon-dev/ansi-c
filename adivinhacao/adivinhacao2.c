012c986e2834785c589a77a0bd3523f2fef16f23
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-10-10 13:52:28 -0300

    Inclui ascii art na abertura e na conclusão do jogo

--- adivinhacao/adivinhacao2.c
+++ adivinhacao/adivinhacao2.c
@@ -4,9 +4,15 @@
 
 int main(){
     // imprime cabeçalho do jogo
-    printf("******************************************\n");
-    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
-    printf("******************************************\n");
+    printf("\n\n");
+    printf("          P  /_\\  P                               \n");
+    printf("         /_\\_|_|_/_\\                             \n");
+    printf("     n_n | ||. .|| | n_n         Bem vindo ao      \n");
+    printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!  \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                      \n");
+    printf("    |_____| ' _ ' |_____|                          \n");
+    printf("          \\__|_|__/                               \n");
+    printf("\n\n");
 
     // recupera o número de segundos a partir de 01/01/1970 EPOCH Time
     int segundos = time(0);
@@ -19,9 +25,26 @@ int main(){
     int chute;
     int tentativas = 1;
     double pontos = 1000;
-
     int acertou = 0;
-    int numerodetentativas = 5;
+
+    int nivel;
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int numerodetentativas;
+
+    switch(nivel){
+        case 1:
+            numerodetentativas = 20;
+            break;
+        case 2:
+            numerodetentativas = 15;
+            break;
+        default:
+            numerodetentativas = 6;
+    }
 
     for (int i = 1; i <= numerodetentativas; i++){
         printf("Tentativa %d de %d\n", tentativas, numerodetentativas);
@@ -57,7 +80,31 @@ int main(){
         printf("Você ganhou!\n");
         printf("Você acertou em %d tentativas!\n", tentativas);
         printf("Total de pontos: %.2f\n", pontos);
+        printf("\n\n");
+        printf("             OOOOOOOOOOO               \n");
+        printf("         OOOOOOOOOOOOOOOOOOO           \n");
+        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+        printf("    OOOOOO      OOOOO      OOOOOO      \n");
+        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+        printf("         OOOOOO         OOOOOO         \n");
+        printf("             OOOOOOOOOOOO              \n");
+        printf("\n\n");
     } else {
         printf("Você perdeu! O número secreto era %d! Tente de novo!\n", numerosecreto);
+
+        printf("\n\n");
+        printf("       \\|/ ____ \\|/       \n");
+        printf("        @~/ ,. \\~@         \n");
+        printf("       /_( \\__/ )_\\       \n");
+        printf("          \\__U_/           \n");
+        printf("\n\n");
     }
 }
